85cfeae65913683c3cca1753c4fda84adb03746a
srvaloehr <srvaloehr@gmail.com>
2025-07-16 22:26:26 -0700

    j

---
+++ week02/styles/bom.js
@@ -0,0 +1,19 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('______'); 
+const li = document.createElement('li');
+const deleteButton = document.createElement('button');
+li.textContent = input.value;
+deleteButton.textContent = '❌';
+li.append(deleteButton);
+list.append(li);
+button.addEventListener('click', function () {
+    // Code to execute when the button is clicked
+});
+if (input.value.trim() !== '') { ... }
+deleteButton.addEventListener('click', function () {
+  list.removeChild(li);
+  input.focus();
+});
+input.value = '';
+input.focus();
