f920457d24adb7c75d1dfc383306d1a86414f4eb
srvaloehr <srvaloehr@gmail.com>
2025-07-17 12:51:23 -0700

    up

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,31 +2,39 @@ const currentYear = document.querySelector("#currentyear");
 const lastModified = document.querySelector("#lastModified");
 
 const header = document.querySelector("header");
-const hamburgerMenuIcon = document.querySelector(".hamburger-menu-icon");
-const navTag = document.querySelector("nav");
-const xmark = document.querySelector(".close-icon");
+const menuIcon = document.querySelector(".hamburger-menu-icon");
+const closeIcon = document.querySelector(".close-icon");
+const nav = document.querySelector("nav");
 
 const today = new Date();
 
-currentYear.innerHTML = `${today.getFullYear()}`;
-lastModified.innerHTML = `${document.lastModified}`;
+currentYear.textContent = today.getFullYear();
+lastModified.textContent = document.lastModified;
 
-hamburgerMenuIcon.addEventListener("click", () => {
-  navTag.classList.toggle("open");
-  xmark.classList.toggle("show");
+menuIcon.addEventListener("click", () => {
+  nav.classList.toggle("open");
+  closeIcon.classList.toggle("show");
 });
 
-xmark.addEventListener("click", () => {
-  navTag.classList.toggle("open");
-  xmark.classList.toggle("show");
+closeIcon.addEventListener("click", () => {
+  nav.classList.toggle("open");
+  closeIcon.classList.toggle("show");
 });
 
 window.addEventListener("resize", () => {
   if (window.innerWidth >= 900) {
-      header.removeChild(hamburgerMenuIcon);
-      header.removeChild(xmark)
+    if (header.contains(menuIcon)) {
+      header.removeChild(menuIcon);
+    }
+    if (header.contains(closeIcon)) {
+      header.removeChild(closeIcon);
+    }
   } else {
-      header.appendChild(hamburgerMenuIcon);
-      header.appendChild(xmark)
+    if (!header.contains(menuIcon)) {
+      header.appendChild(menuIcon);
+    }
+    if (!header.contains(closeIcon)) {
+      header.appendChild(closeIcon);
+    }
   }
 });
