ef979003192f96949c8f674673bf33649fa5f08e
srvaloehr <srvaloehr@gmail.com>
2025-08-09 19:13:50 -0700

    ahh

--- scripts/form.js
+++ scripts/form.js
@@ -8,28 +8,28 @@ lastModified.innerHTML = `${document.lastModified}`;
 
 const products = [
   {
-    id: "fc-1888",
-    name: "flux capacitor",
+    id: "DC-187",
+    name: "Barbell",
     averagerating: 4.5,
   },
   {
-    id: "fc-2050",
-    name: "power laces",
+    id: "DB-656",
+    name: "S&W .357",
     averagerating: 4.7,
   },
   {
-    id: "fs-1987",
-    name: "time circuits",
+    id: "KB-901",
+    name: "Kettlebell",
     averagerating: 3.5,
   },
   {
-    id: "ac-2000",
-    name: "low voltage reactor",
+    id: "CB-900",
+    name: "Cable Pulley",
     averagerating: 3.9,
   },
   {
-    id: "jj-1969",
-    name: "warp equalizer",
+    id: "GL-160",
+    name: "Canik Mete",
     averagerating: 5.0,
   },
 ];
